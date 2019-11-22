0b1113304186e01c6273062606f2050c7ddb1553
btieu1 <56889945+btieu1@users.noreply.github.com>
2019-11-22 12:12:13 -0500

    Update Warddrobe.java

--- Warddrobe.java
+++ Warddrobe.java
@@ -12,6 +12,18 @@ public class Warddrobe{
    private ArrayList<Pants> pants;
    private ArrayList<Sock> socks;
    private ArrayList<Shoe> shoes;
+   private List<List<Clothing>> wardrobe; 
+   private List<List<Clothing>> sWardrobe; 
+   
+   private ArrayList<Hat> sHats;
+   private ArrayList<Scarf> sScarves;
+   private ArrayList<Glasses> sGlasses;
+   private ArrayList<Shirt> sShirts;
+   private ArrayList<Jacket> sJackets;
+   private ArrayList<Glove> sGloves;
+   private ArrayList<Pants> sPants;
+   private ArrayList<Sock> sSocks;
+   private ArrayList<Shoe> sShoes;
    
    //constructor takes in arraylists
    public Warddrobe(ArrayList<Hat> hats, ArrayList<Scarf> scarves,ArrayList<Glasses> glasses, ArrayList<Shirt> shirts, ArrayList<Jacket> jackets, ArrayList<Glove> gloves, ArrayList<Pants> pants, ArrayList<Sock> socks, ArrayList<Shoe> shoes){
@@ -24,6 +36,32 @@ public class Warddrobe{
       this.pants = pants;
       this.socks = socks;
       this.shoes = shoes;
+      
+      this.wardrobe = new ArrayList<List<Clothing>>(); //TO CHECK IF RIGHT ******************************
+   }
+   
+   
+   public void Condense(String occasion, boolean attribute2, boolean attribute3, boolean attribute4){
+      this.sHats = new ArrayList<Hat>();
+      this.sScarves = new ArrayList<Scarf>();
+      this.sGlasses = new ArrayList<Glasses>();
+      this.sShirts = new ArrayList<Shirt>();
+      this.sJackets = new ArrayList<Jacket>();
+      this.sGloves = new ArrayList<Glove>();
+      this.sPants = new ArrayList<Pants>();
+      this.sSocks = new ArrayList<Sock>();
+      this.sShoes = new ArrayList<Shoe>();
+      
+      this.sWardrobe = new ArrayList<List<Clothing>>(); //TO CHECK IF RIGHT ******************************
+
+      
+      for(int i = 0; i < this.wardrobe.size(); i++){
+         for(int j = 0; j < this.wardrobe.size(); j++){ //TO CHECK IF RIGHT ******************************
+            if(this.wardrobe.get(i).get(j).getOccasion() = ocassion && this.wardrobe.get(i).get(j).getAttribute2() = attribute2){
+               this.sWardrobe.get(i).add();
+            }
+            
+         }
+      }
    }
 }
-   
