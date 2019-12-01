3a5482758b1b563a5756bb773fd83df3f6e8c650
bub-dev <57117919+bub-dev@users.noreply.github.com>
2019-12-01 16:06:53 -0500

    Update Sock.java

--- Sock.java
+++ Sock.java
@@ -4,8 +4,19 @@ public class Sock extends Clothing{
    //attributes or variables specific to sock (Still Working on)
    
    //constructor
-   public Sock(boolean stackable, boolean permanent, String season, String color, String occasion, String material, String type, String ID){
-      super(stackable, permanent, season, color, occasion, material, type, ID);
+   public Sock(String stackable, String permanent, String season, String color, String occasion, String type, String ID){
+      super(stackable, permanent, season, color, occasion, type, ID);
    }
    
+   public String getStackable(){
+      return this.stackable;
+   }
+   
+   public String getPermanent(){
+      return this.permanent;
+   }
+   
+   public String getOccasion(){
+      return this.occasion;
+   }
 }
