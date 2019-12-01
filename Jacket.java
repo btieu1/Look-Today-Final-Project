d16f88eeaee67f851f0f2759624d01e6171604cc
btieu1 <56889945+btieu1@users.noreply.github.com>
2019-12-01 10:24:22 -0500

    Update Jacket.java

--- Jacket.java
+++ Jacket.java
@@ -3,8 +3,8 @@ public class Jacket extends Clothing{
    //attributes or variables specific to Jackets (Still Working on)
    
    //constructor
-   public Jacket(boolean stackable, boolean permanent, String season, String color, String occasion, String material){
-      super(stackable, permanent, season, color, occasion, material);
+   public Jacket(boolean stackable, boolean permanent, String season, String color, String occasion, String material, String type, String ID){
+      super(stackable, permanent, season, color, occasion, material, type, ID);
    }
    
 }
