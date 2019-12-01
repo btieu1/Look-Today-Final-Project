33f2c0558522974810f3bb0644c4bdbca9ed413d
bub-dev <57117919+bub-dev@users.noreply.github.com>
2019-12-01 16:02:33 -0500

    Update Glove.java

--- Glove.java
+++ Glove.java
@@ -3,8 +3,19 @@ public class Glove extends Clothing{
    //attributes or variables specific to gloves (Still Working on)
    
    //constructor
-   public Glove(boolean stackable, boolean permanent, String season, String color, String occasion, String material, String type, String ID){
-      super(stackable, permanent, season, color, occasion, material, type, ID);
+   public Glove(String stackable, String permanent, String season, String color, String occasion, String type, String ID){
+      super(stackable, permanent, season, color, occasion, type, ID);
    }
    
+   public String getStackable(){
+      return this.stackable;
+   }
+   
+   public String getPermanent(){
+      return this.permanent;
+   }
+   
+   public String getOccasion(){
+      return this.occasion;
+   }
 }
