427a07c4377e6775e8242e8a8dabcf8b38adb96c
btieu1 <56889945+btieu1@users.noreply.github.com>
2019-12-01 10:26:45 -0500

    Update Warddrobe.java

--- Warddrobe.java
+++ Warddrobe.java
@@ -12,8 +12,8 @@ public class Warddrobe{
    private ArrayList<Pants> pants;
    private ArrayList<Sock> socks;
    private ArrayList<Shoe> shoes;
-   private List<List<Clothing>> wardrobe; 
-   private List<List<Clothing>> sWardrobe; 
+   private List<Clothing> wardrobe; 
+   private List<Clothing> sWardrobe; 
    
    private ArrayList<Hat> sHats;
    private ArrayList<Scarf> sScarves;
@@ -37,11 +37,41 @@ public class Warddrobe{
       this.socks = socks;
       this.shoes = shoes;
       
-      this.wardrobe = new ArrayList<List<Clothing>>(); //TO CHECK IF RIGHT ******************************
+      this.wardrobe = new ArrayList<Clothing>();
+      
+      //adds all the clothing items into the wardrobe array
+      for(int i = 0; i<hats.size(); i++){
+         this.wardrobe.add(hats.get(i));
+      }
+      for(int i = 0; i<scarves.size(); i++){
+         this.wardrobe.add(scarves.get(i));
+      }
+      for(int i = 0; i<glasses.size(); i++){
+         this.wardrobe.add(glasses.get(i));
+      }
+      for(int i = 0; i<shirts.size(); i++){
+         this.wardrobe.add(shirts.get(i));
+      }
+      for(int i = 0; i<jackets.size(); i++){
+         this.wardrobe.add(jackets.get(i));
+      }
+      for(int i = 0; i<gloves.size(); i++){
+         this.wardrobe.add(gloves.get(i));
+      }
+      for(int i = 0; i<pants.size(); i++){
+         this.wardrobe.add(pants.get(i));
+      }
+      for(int i = 0; i<socks.size(); i++){
+         this.wardrobe.add(socks.get(i));
+      }
+      for(int i = 0; i<shoes.size(); i++){
+         this.wardrobe.add(shoes.get(i));
+      }
+      
    }
    
    
-   public void Condense(String occasion, boolean attribute2, boolean attribute3, boolean attribute4){
+   public void Condense(String occasion, String season, String primeColor){
       this.sHats = new ArrayList<Hat>();
       this.sScarves = new ArrayList<Scarf>();
       this.sGlasses = new ArrayList<Glasses>();
@@ -52,16 +82,28 @@ public class Warddrobe{
       this.sSocks = new ArrayList<Sock>();
       this.sShoes = new ArrayList<Shoe>();
       
-      this.sWardrobe = new ArrayList<List<Clothing>>(); //TO CHECK IF RIGHT ******************************
+      this.sWardrobe = new ArrayList<Clothing>();
 
       
-      for(int i = 0; i < this.wardrobe.size(); i++){
-         for(int j = 0; j < this.wardrobe.size(); j++){ //TO CHECK IF RIGHT ******************************
-            if(this.wardrobe.get(i).get(j).getOccasion() = ocassion && this.wardrobe.get(i).get(j).getAttribute2() = attribute2){
-               this.sWardrobe.get(i).add();
+         for(int j = 0; j < this.wardrobe.size(); j++){
+            if(this.wardrobe.get(j).getOccasion().equals(occasion) && this.wardrobe.get(j).getSeason().equals(season)){
+               this.sWardrobe.add(this.wardrobe.get(j));
             }
-            
-         }
       }
+   }
+   
+   public String toString(){
+       System.out.println("Condensed clothes:");
+      for(int i = 0; i<this.sWardrobe.size(); i++){
+         System.out.print(this.sWardrobe.get(i)+", ");
+         }
+         
+         System.out.println("");
+       System.out.println("Your closet:"); 
+       for(int i = 0; i<this.wardrobe.size(); i++){ 
+         System.out.print(this.wardrobe.get(i)+", ");
+         }
+         
+      return "";
    }
 }
