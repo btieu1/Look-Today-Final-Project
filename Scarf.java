d91877a581459b2c2957d3376e015ef1c9cf4b21
btieu1 <56889945+btieu1@users.noreply.github.com>
2019-12-01 10:25:22 -0500

    Update Scarf.java

--- Scarf.java
+++ Scarf.java
@@ -3,8 +3,8 @@ public class Scarf extends Clothing{
    //attributes or variables specific to scarves (Still Working on)
    
    //constructor
-   public Scarf(boolean stackable, boolean permanent, String season, String color, String occasion, String material){
-      super(stackable, permanent, season, color, occasion, material);
+   public Scarf(boolean stackable, boolean permanent, String season, String color, String occasion, String material, String type, String ID){
+      super(stackable, permanent, season, color, occasion, material, type, ID);
    }
    
 }
