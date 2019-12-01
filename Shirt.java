c4b0673d3817f48c14eff777f9bcf30356b2ed28
btieu1 <56889945+btieu1@users.noreply.github.com>
2019-12-01 10:25:38 -0500

    Update Shirt.java

--- Shirt.java
+++ Shirt.java
@@ -3,8 +3,8 @@ public class Shirt extends Clothing{
    //attributes or variables specific to Shirts (Still Working on)
    
    //constructor
-   public Shirt(boolean stackable, boolean permanent, String season, String color, String occasion, String material){
-      super(stackable, permanent, season, color, occasion, material);
+   public Shirt(boolean stackable, boolean permanent, String season, String color, String occasion, String material, String type, String ID){
+      super(stackable, permanent, season, color, occasion, material, type, ID);
    }
    
 }
