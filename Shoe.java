f3c4ed5a620cc156b515138ccfab4be87912a46e
bub-dev <57117919+bub-dev@users.noreply.github.com>
2019-12-01 16:06:23 -0500

    Update Shoe.java

--- Shoe.java
+++ Shoe.java
@@ -3,8 +3,19 @@ public class Shoe extends Clothing{
    //attributes or variables specific to shoes (Still Working on)
    
    //constructor
-   public Shoe(boolean stackable, boolean permanent, String season, String color, String occasion, String material, String type, String ID){
-      super(stackable, permanent, season, color, occasion, material, type, ID);
+   public Shoe(String stackable, String permanent, String season, String color, String occasion, String type, String ID){
+      super(stackable, permanent, season, color, occasion, type, ID);
    }
    
+   public String getStackable(){
+      return this.stackable;
+   }
+   
+   public String getPermanent(){
+      return this.permanent;
+   }
+   
+   public String getOccasion(){
+      return this.occasion;
+   }
 }
