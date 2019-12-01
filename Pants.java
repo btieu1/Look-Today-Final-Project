5d6d33f75887f5e8640d3dcdaef23ce192f5a129
btieu1 <56889945+btieu1@users.noreply.github.com>
2019-12-01 10:24:57 -0500

    Update Pants.java

--- Pants.java
+++ Pants.java
@@ -3,8 +3,8 @@ public class Pants extends Clothing{
    //attributes or variables specific to pants (Still Working on)
    
    //constructor
-   public Pants(boolean stackable, boolean permanent, String season, String color, String occasion, String material){
-      super(stackable, permanent, season, color, occasion, material);
+   public Pants(boolean stackable, boolean permanent, String season, String color, String occasion, String material, String type, String ID){
+      super(stackable, permanent, season, color, occasion, material, type, ID);
    }
    
 }
