24a2d41b487f946832f701f2dc4efeb1eee18e08
btieu1 <56889945+btieu1@users.noreply.github.com>
2019-12-01 10:23:59 -0500

    Update Hat.java

--- Hat.java
+++ Hat.java
@@ -3,8 +3,8 @@ public class Hat extends Clothing{
    //attributes or variables specific to Hats (Still Working on)
    
    //constructor
-   public Hat(boolean stackable, boolean permanent, String season, String color, String occasion, String material){
-      super(stackable, permanent, season, color, occasion, material);
+   public Hat(boolean stackable, boolean permanent, String season, String color, String occasion, String material, String type, String ID){
+      super(stackable, permanent, season, color, occasion, material, type, ID);
    }
    
 }
