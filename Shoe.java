2b6e585cb3ff5a71e1dc7b3f15fcf935d4740c82
btieu1 <56889945+btieu1@users.noreply.github.com>
2019-12-01 10:26:03 -0500

    Update Shoe.java

--- Shoe.java
+++ Shoe.java
@@ -3,8 +3,8 @@ public class Shoe extends Clothing{
    //attributes or variables specific to shoes (Still Working on)
    
    //constructor
-   public Shoe(boolean stackable, boolean permanent, String season, String color, String occasion, String material){
-      super(stackable, permanent, season, color, occasion, material);
+   public Shoe(boolean stackable, boolean permanent, String season, String color, String occasion, String material, String type, String ID){
+      super(stackable, permanent, season, color, occasion, material, type, ID);
    }
    
 }
